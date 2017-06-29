bdf95d7a2303d4c60581384cb903135983b5bed1
Madison <ostenant@163.com>
2017-06-29 22:29:47 +0800

    Add controller and service for springboot-mybatis-mixedtype.

---
+++ springboot-mybatis-mixedtype/src/main/java/org/ostenant/springboot/learning/examples/service/StudentService.java
@@ -0,0 +1,9 @@
+package org.ostenant.springboot.learning.examples.service;
+
+import org.ostenant.springboot.learning.examples.model.Student;
+
+/**
+ * Created by Madison on 2017/6/29.
+ */
+public interface StudentService extends BasicDaoSupport<Student, String> {
+}
