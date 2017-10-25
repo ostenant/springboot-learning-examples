bd236dafd57e9d898a04f65dc444a172bf22e153
Madison <ostenant@163.com>
2017-10-25 20:56:27 +0800

    Feat: add springboot-rpc motan annotation module

--- springboot-rpc-motan/springboot-rpc-motan-annotation/springboot-rpc-motan-annotation-service2/src/main/java/org/ostenant/springboot/learning/examples/Application.java
+++ springboot-rpc-motan/springboot-rpc-motan-annotation/springboot-rpc-motan-annotation-service2/src/main/java/org/ostenant/springboot/learning/examples/Application.java
@@ -4,10 +4,8 @@ import com.weibo.api.motan.common.MotanConstants;
 import com.weibo.api.motan.util.MotanSwitcherUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource(locations = {"classpath:motan-server-zookeeper.xml"})
 public class Application {
 
     public static void main(String[] args) {
--- springboot-rpc-motan/springboot-rpc-motan-xml/springboot-rpc-motan-xml-service1/src/main/java/org/ostenant/springboot/learning/examples/Application.java
+++ springboot-rpc-motan/springboot-rpc-motan-xml/springboot-rpc-motan-xml-service1/src/main/java/org/ostenant/springboot/learning/examples/Application.java
@@ -2,20 +2,17 @@ package org.ostenant.springboot.learning.examples;
 
 import com.weibo.api.motan.common.MotanConstants;
 import com.weibo.api.motan.util.MotanSwitcherUtil;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource(locations = "classpath:motan-server-zookeeper.xml")
-@Slf4j
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         // 心跳健康检查
         MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
-        log.info("Motan Server Start ...");
     }
 }
